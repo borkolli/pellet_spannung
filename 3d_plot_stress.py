f280558aeb1726575f7d6b5d49ca89d3a8df5199
hofnil <noff@posteo.de>
2024-01-04 21:50:27 +0100

    Add files via upload

---
+++ 3d_plot_stress.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+input_filename = 'datasets/1run_225_end_compression_particle-data.csv'
+
+# Read the CSV into a pandas DataFrame
+df = pd.read_csv(input_filename, skiprows=[1])
+
+new_column_names = {
+    'Tensile Stress': 'stress_tensile',
+    'Particle ID From': 'id_from',
+    'Shear Stress': 'stress_shear',
+    'Coordinate : Y': 'pos_y',
+    'von Mises Stress': 'stress_mises',
+    'Coordinate : X': 'pos_x',
+    'Coordinate : Z': 'pos_z',
+    'Particle ID To': 'id_to',
+}
+
+df.rename(columns=new_column_names, inplace=True)
+
+columns_to_drop = ['id_from', 'id_to', 'stress_mises', 'stress_shear']
+df.drop(columns=columns_to_drop, inplace=True)
+
+desired_order = ['pos_x', 'pos_y', 'pos_z', 'stress_tensile']
+
+df = df[desired_order].copy()
+
+df['pos_x'] *= 1e6
+df['pos_y'] *= 1e6
+df['pos_z'] *= 1e6
+df['radius'] = (df['pos_x']**2 + df['pos_y']**2).apply(lambda x: x**0.5)
+
+df_high_stress = df[df.stress_tensile >= df.stress_tensile.max() * 0.3]
+
+# Create a 3D plot
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+# Scatter plot with pos_x, pos_y, pos_z as coordinates and stress_tensile as color
+scatter = ax.scatter(df_high_stress['pos_x'], df_high_stress['pos_y'], df_high_stress['pos_z'],
+                     c=df_high_stress['stress_tensile'], cmap='viridis')
+
+# Add labels
+ax.set_xlabel('X [µm]')
+ax.set_ylabel('Y [µm]')
+ax.set_zlabel('Z [µm]')
+
+# Add colorbar
+cbar = fig.colorbar(scatter, ax=ax, label='Zugspannung')
+
+# Show the plot
+plt.show()
